ed657f6c4d6be8968784c198d3349748d19d8479
bespalovnikita <bespalovnik@gmail.com>
2019-01-11 12:46:31 +0600

    Исправление

--- JDBC,JSTL/src/main/java/Log2Filter.java
+++ JDBC,JSTL/src/main/java/Log2Filter.java
@@ -18,6 +18,7 @@ public class Log2Filter implements Filter {
     public void init(FilterConfig fConfig) throws ServletException {
         this.logFile = fConfig.getInitParameter("logFile");
         System.out.println("Log File " + logFile);
+
     }
 
     @Override
